8cb9cb3230ae92bd74174daae4534fe67f39aa4e
Elijah Toppo <elijah981@gmail.com>
2022-01-21 00:24:32 +0530

    make create_app

--- website/__init__.py
+++ website/__init__.py
@@ -1,40 +1,40 @@
-import os
-import json
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_sqlalchemy import SQLAlchemy
+from website.config import Config
 
-with open("/etc/config.json") as config_file:
-    config = json.load(config_file)
 
-app = Flask(__name__)
-app.config["SECRET_KEY"] = config.get("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = config.get("SQLALCHEMY_DATABASE_URI")
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
 login_manager.login_view = "users.login"
 login_manager.login_message_category = "info"
-app.config["MAIL_SERVER"] = "smtp.googlemail.com"
-app.config["MAIL_PORT"] = 587
-app.config["MAIL_USE_TLS"] = True
-app.config["MAIL_USERNAME"] = config.get("EMAIL_USER")
-app.config["MAIL_PASSWORD"] = config.get("EMAIL_PASS")
-mail = Mail(app)
-
-from website.users.routes import users
-from website.posts.routes import posts
-from website.main.routes import main
-from website.accounts.routes import accounts
-from website.budgets.routes import budgets
-from website.transactions.routes import transactions
+mail = Mail()
 
 
-app.register_blueprint(users)
-app.register_blueprint(posts)
-app.register_blueprint(main)
-app.register_blueprint(accounts)
-app.register_blueprint(budgets)
-app.register_blueprint(transactions)
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+
+    from website.accounts.routes import accounts
+    from website.budgets.routes import budgets
+    from website.main.routes import main
+    from website.posts.routes import posts
+    from website.transactions.routes import transactions
+    from website.users.routes import users
+
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    app.register_blueprint(main)
+    app.register_blueprint(accounts)
+    app.register_blueprint(budgets)
+    app.register_blueprint(transactions)
+
+    return app
