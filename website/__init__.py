dd4976c3a1afb1e9a5bf24110e3c54b879063890
Elijah Toppo <elijah981@gmail.com>
2022-01-20 13:58:34 +0000

    update init file and some profile pics

--- website/__init__.py
+++ website/__init__.py
@@ -1,13 +1,17 @@
 import os
+import json
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
 
+with open('/etc/config.json') as config_file:
+	config = json.load(config_file)
+
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "5791628bb0b13ce0c676dfde280ba245"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
+app.config["SECRET_KEY"] = config.get('SECRET_KEY')
+app.config["SQLALCHEMY_DATABASE_URI"] = config.get('SQLALCHEMY_DATABASE_URI')
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
@@ -16,8 +20,8 @@ login_manager.login_message_category = "info"
 app.config["MAIL_SERVER"] = "smtp.googlemail.com"
 app.config["MAIL_PORT"] = 587
 app.config["MAIL_USE_TLS"] = True
-app.config["MAIL_USERNAME"] = os.environ.get("EMAIL_USER")
-app.config["MAIL_PASSWORD"] = os.environ.get("EMAIL_PASS")
+app.config["MAIL_USERNAME"] = config.get("EMAIL_USER")
+app.config["MAIL_PASSWORD"] = config.get("EMAIL_PASS")
 mail = Mail(app)
 
 from website import routes
