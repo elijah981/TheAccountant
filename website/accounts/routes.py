f5be7043ba25398c46df384bf28449ae37c92a08
Elijah Toppo <elijah981@gmail.com>
2022-01-23 00:20:57 +0530

    Update account feature added

--- website/accounts/routes.py
+++ website/accounts/routes.py
@@ -1,8 +1,11 @@
-from flask import Blueprint, flash, redirect, render_template, url_for
+from datetime import datetime
+
+from flask import Blueprint, flash, redirect, render_template, request, url_for
 from flask_login import current_user, login_required
 from website import db
 from website.accounts.forms import AccountForm
 from website.models import Account
+from wtforms import SubmitField
 
 accounts = Blueprint("accounts", __name__)
 
@@ -44,3 +47,40 @@ def new_account():
 def read_acc(acc_id):
     acc = Account.query.filter_by(user_id=current_user.id).get_or_404(acc_id)
     return render_template("acc.html", title=acc.name, post=acc)
+
+
+@accounts.route("/accounts/<int:acc_id>/update", methods=["GET", "POST"])
+@login_required
+def update_account(acc_id):
+    account = Account.query.filter_by(id=acc_id).first()
+    if account.user_id != current_user.id:
+        abort(403)
+    form = AccountForm()
+    if form.validate_on_submit():
+        account.name = form.name.data
+        account.type = form.type.data
+        account.bank = form.bank.data
+        account.acc_num = form.acc_num.data
+        account.credit_card = form.credit_card.data
+        account.investment = form.investment.data
+        account.amount = form.amount.data
+        account.date = datetime.utcnow()
+        db.session.commit()
+        flash("Your account has been updated!", "success")
+        return redirect(url_for("accounts.show_accounts"))
+    elif request.method == "GET":
+        form.name.data = account.name
+        form.type.data = account.type
+        form.bank.data = account.bank
+        form.acc_num.data = account.acc_num
+        form.credit_card.data = account.credit_card
+        form.investment.data = account.investment
+        form.amount.data = account.amount
+        form.submit.label.text = "Update"
+
+    return render_template(
+        "create_account.html",
+        title="Update Account",
+        form=form,
+        legend="Update Account",
+    )
