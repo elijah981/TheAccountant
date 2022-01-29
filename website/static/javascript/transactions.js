3fa4f60452eeeead7cddd8faf69ad65b571804bf
Elijah Toppo <elijah981@gmail.com>
2022-01-29 11:34:20 +0530

    Get options in transactions

--- website/static/javascript/transactions.js
+++ website/static/javascript/transactions.js
@@ -1,12 +1,108 @@
 
+var account_options = getAccountOptions();
+var budget_options = getBudgetOptions();
+
+function getAccountOptions() {
+    var result = null;
+    $.ajax({
+        async: false,
+        url: "api/accounts",
+        type: "GET",
+        dataType: "json",
+        success: function (data) {
+            result = data["data"];
+        }
+    });
+
+    console.log(result);
+
+    var i;
+    var name = {}
+    var id = {}
+    for (i = 0; i < result.length; i++) {
+        name[result[i]["id"]] = result[i]["name"];
+        id[result[i]["id"]] = result[i]["id"];
+    }
+    var data = [name, id];
+    return data;
+}
+
+function getBudgetOptions() {
+    var result = null;
+    $.ajax({
+        async: false,
+        url: "api/budgets",
+        type: "GET",
+        dataType: "json",
+        success: function (data) {
+            result = data["data"];
+        }
+    });
+
+    console.log(result);
+
+    var i;
+    var name = {}
+    var id = {}
+    for (i = 0; i < result.length; i++) {
+        name[result[i]["id"]] = result[i]["name"];
+    }
+
+    return name;
+}
+
+var minDate, maxDate;
+
+// Custom filtering function which will search data in column four between two values
+$.fn.dataTable.ext.search.push(
+    function (settings, data, dataIndex) {
+        var min = minDate.val();
+        var max = maxDate.val();
+        var date = new Date(data[4]);
+
+        if (
+            (min === null && max === null) ||
+            (min === null && date <= max) ||
+            (min <= date && max === null) ||
+            (min <= date && date <= max)
+        ) {
+            return true;
+        }
+        return false;
+    }
+);
+
 $(document).ready(function () {
+
+    // Create date inputs
+    minDate = new DateTime($('#min'), {
+        format: 'MMMM Do YYYY'
+    });
+    maxDate = new DateTime($('#max'), {
+        format: 'MMMM Do YYYY'
+    });
 
     var columnDefs = [
         { "data": "id", "visible": false, "disabled": true },
         { "data": "date" },
-        { "data": "from_name" },
-        { "data": "to_name" },
-        { "data": "txn_tag" },
+        {
+            "data": "from_name",
+            "type": "select",
+            "select2": { width: "100%" },
+            "options": account_options[0],
+        },
+        {
+            "data": "to_name",
+            "type": "select",
+            "select2": { width: "100%" },
+            "options": account_options[0],
+        },
+        {
+            "data": "txn_tag",
+            "type": "select",
+            "select2": { width: "100%" },
+            "options": budget_options,
+        },
         { "data": "description" },
         { "data": "amount" },
         // { "data": "project_id", "visible": false, "disabled": true },
